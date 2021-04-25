bae81de38c8002fe22924cade722b215505ec73a
Zack Kimble <zkimble@gmail.com>
2021-04-25 13:49:08 -0400

    changes build_image.sh to use curl instead of wget

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
-wget https://github.com/foamliu/InsightFace-v2/releases/download/v1.0/BEST_checkpoint_r101.tar app/insightface/BEST_checkpoint_r101.tar
+curl https://github.com/foamliu/InsightFace-v2/releases/download/v1.0/BEST_checkpoint_r101.tar app/insightface/BEST_checkpoint_r101.tar
 docker-compose build
